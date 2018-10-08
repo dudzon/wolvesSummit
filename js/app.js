32701f13ad7cab5a4bc647733d2060cbb9c84c5d
Tomasz Dudzinski <tomasz.j.p.dudzinski@gmail.com>
2018-10-08 17:17:42 +0200

    Dropddown menus- desktop

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const agenda = document.querySelectorAll('.header__navigation-item--dropdown')[1];
-console.log(agenda);
+const whyAttend = document.querySelectorAll('.header__navigation-item--dropdown')[0];
 
+// Agenda dropdown menu
 agenda.addEventListener('click',function(){
 
     let hiddenMenu = this.children[1];
@@ -15,3 +16,43 @@ agenda.addEventListener('click',function(){
         hiddenMenu.classList.add('dropdown--not-visible');
     }
 },false)
+
+// Why Attend dropdown menu
+
+whyAttend.addEventListener('click',function(e){
+    const header = document.querySelector('.header');
+    const bottomMenu = document.createRange().createContextualFragment(`<section class="why-attend">
+<div class="why-attend__container">
+    <ul class="why-attend__business">
+        <li>Business</li>
+        <li><a href="#">Find Innovation</a></li>
+        <li><a href="#">Find Clients</a></li>
+    </ul>
+    <ul class="why-attend__startups">
+        <li>Startups</li>
+        <li><a href="#">Attend for free</a></li>
+        <li><a href="#">Attend as scaleup</a></li>
+    </ul>
+    <ul class="why-attend__investors">
+        <li>Investor</li>
+        <li><a href="#">Find Investment</a></li>
+    </ul>
+    <ul class="why-attend__last">
+        <li>And</li>
+        <li><a href="#">Volunteer</a></li>
+        <li><a href="#">Speaker</a></li>
+        <li><a href="#">Media</a></li>
+    </ul>
+</div>
+</section>`);
+
+    if( e.target.closest('.header').nextElementSibling.tagName !=='SECTION' ){
+        this.style.background = '#f4f4f4';
+        header.after(bottomMenu);
+    } else{
+        this.style.background = '#fff';
+        e.target.closest('.header').parentElement.children[3].remove();
+    }
+})
+
+
