b89f71057ed2ce2bdc5bc6c26ff3c226586278a5
Tomasz Dudzinski <tomasz.j.p.dudzinski@gmail.com>
2018-10-10 15:17:54 +0200

    JS refactor v.2.0

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ function isWhyAttendVisible(){
         whyAttend.style.background = '';
     }
 }
-//  Create  navigation down menu 
+//  Create  navigation down menu
 
 function createMenu(){
     const bottomMenu = document.createRange().createContextualFragment(`<section class="why-attend">
